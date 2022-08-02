249304708a8ab75a34e717b944ff1ca752505089
Min Lwin <lwin.zawmin@gmail.com>
2022-08-02 14:24:21 +0630

    Add Member Module

--- projects/lazyapp/src/app/app.component.ts
+++ projects/lazyapp/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SecurityContext } from './services/security/sercurity-context';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styles: []
 })
-export class AppComponent {
-  title = 'lazyapp';
+export class AppComponent implements OnInit {
+
+  constructor(private context:SecurityContext, private router:Router) {}
+
+  ngOnInit(): void {
+    this.router.navigate([this.context.security?.role.toLocaleLowerCase() || 'public'])
+  }
 }
